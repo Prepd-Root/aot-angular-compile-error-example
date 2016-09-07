e723b675db32e6e4eaaf426cbd196e7b75c902a0
Prepd-Root <prepd.master@gmail.com>
2016-09-07 16:18:39 +0300

    Create app.component.ts

---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'playground-app',
+  template: `
+    <pre>Playground</pre>
+    <playground-form></playground-form>
+  `
+})
+export class AppComponent { }
